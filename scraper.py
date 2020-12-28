e5bff259bcd06e3474287ed7c3814491305af789
Steven Zinck <steve@zinck.ca>
2020-12-28 16:54:33 -0400

    initial commit

---
+++ scraper.py
@@ -0,0 +1,83 @@
+from folium.plugins import MarkerCluster
+import folium
+import requests
+import re
+
+
+import geopy
+from geopy.geocoders import Nominatim
+from bs4 import BeautifulSoup
+
+def getData():
+
+    URL = 'https://www.halifax.ca/recreation/programs-activities/skating/ice-thickness'
+
+    try:
+        r = requests.get(URL)
+    except requests.exceptions.RequestException as e: 
+        raise SystemExit(e)
+    soup = BeautifulSoup(r.content, 'lxml')
+
+    table = soup.find("table")
+
+    ice_dict = {}
+    for tr in table.findAll('tr')[1:]:
+
+        col = tr.findAll('td')
+
+        name = str(col[0].string)
+        location = str(col[1].string)
+        status = str(col[3].string)
+        date = str(col[4].string)
+
+        ice_dict[name] = {}
+        ice_dict[name]['Location'] = location
+        ice_dict[name]['Status'] = status
+        ice_dict[name]['Date'] = date
+
+    return ice_dict
+
+
+def geoCoder(location):
+
+    locator = Nominatim(user_agent="mygeocoder")
+
+    geolocation = locator.geocode(location)
+
+    return [geolocation.latitude, geolocation.longitude]
+
+
+def createMapMarker(ice_dict):
+
+    feature_group = folium.FeatureGroup("Locations")
+    my_map = folium.Map(location=[44.651070, -63.582687], zoom_start=8)
+
+    for lake_name in ice_dict:
+        try:
+            
+            lake_status = ice_dict[lake_name]['Status']
+            lake_date = ice_dict[lake_name]['Date']
+            lake_name = re.sub(r'\([^)]*\)', '', lake_name).strip()
+            lake = geoCoder(lake_name + ", Nova Scotia")
+
+            html = f'''<h4>Name:</h4> {lake_name} <br />\
+            <h4>Status: </h4> {lake_status} <br />\
+            <h4>Date: </h4> {lake_date} <br />\
+            '''
+
+            feature_group.add_child(folium.Marker(lake, popup=html))
+
+        except:
+            print("Error with: " + lake_name)
+            continue
+    my_map.add_child(feature_group)
+    my_map.save("public/index.html")
+
+
+#getData()
+createMapMarker(getData())
+
+
+
+
+
